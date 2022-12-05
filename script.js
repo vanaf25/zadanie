ffab4e883554e166578028e84d3b330930db21af
vanay <vanayfefilov777@gmail.com>
2022-12-05 22:53:39 +0100

    init

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const resultContainer=document.querySelector(".result");
 const printNumbers=(number)=>{
     const numbers=[]
+    resultContainer.innerHTML+=`<div>`
     for (let i=number;i<=number+2;i++){
         numbers.push(i)
         resultContainer.innerHTML+=`<span>${i}, </span>`
     }
+    resultContainer.innerHTML+='</div>'
     return numbers
 }
 const calculate=(numbers)=>{
@@ -21,7 +23,11 @@ document.querySelector("#button").addEventListener("click",()=>{
     const value1=parseFloat(document.querySelector("#value1").value);
     const value2=parseFloat(document.querySelector("#value2").value);
     resultContainer.innerHTML=""
-   const numbers1 =printNumbers(value1);
-    const numbers2=printNumbers(value2);
-    calculate([...numbers1,...numbers2]);
+    if (!isNaN(value1) && !isNaN(value2)){
+        const numbers1 =printNumbers(value1);
+        const numbers2=printNumbers(value2);
+        calculate([...numbers1,...numbers2]);
+    }
+    else resultContainer.innerHTML=`Bląd musisz wporowadzić liczby w formularz`
+
 })
